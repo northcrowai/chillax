1ed7c3e652edc75a297f9bc3233b21a5d17ff8d8
Kevin Oskow <kevin@northcrow.ai>
2026-08-06 21:01:48 -0700

    Use North Crow favicon assets

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,8 @@ export default defineConfig({
     VitePWA({
       registerType: 'prompt',
       includeAssets: [
-        'favicon.svg',
+        'favicon-16x16.png',
+        'favicon-32x32.png',
         'apple-touch-icon.png',
         'pwa-192x192.png',
         'pwa-512x512.png',
